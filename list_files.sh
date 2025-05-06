3ed51a80cf0ea158ef748b74da4de0722f79fdeb
Ridouane-ridix <ridix0202@gmail.com>
2025-05-06 19:05:30 -0400

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Ce script affiche la liste des fichiers du répertoire courant
-
-echo "Fichiers dans le répertoire courant :"
-ls -lh    # -l : format détaillé, -h : taille lisible
-
+ls -la  
