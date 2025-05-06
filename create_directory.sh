25877da6cc8b4c4421d8526d5d5993f69da2160f
Ridouane-ridix <ridix0202@gmail.com>
2025-05-06 19:02:39 -0400

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # Ce script crée un répertoire nommé test_directory
-
-DIR="test_directory"
-
-# Vérifie si le répertoire existe déjà
-if [ -d "$DIR" ]; then
-    echo "Le répertoire '$DIR' existe déjà."
-else
-    mkdir "$DIR"
-    echo "Répertoire '$DIR' créé avec succès."
-fi
-
+mkdir -p test_directory
